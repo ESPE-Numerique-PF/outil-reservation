46970c31a116323ccb4a71d5c99cae038f96b739
ESPE NUMERIQUE <espenumerique@espe-boss.adupf.local>
2020-08-28 11:24:25 -1000

    set home as main view

--- routes/web.php
+++ routes/web.php
@@ -14,14 +14,10 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/', function () {
+//     return view('welcome');
+// });
 
 Auth::routes();
 
-Route::get('/home', 'HomeController@index')->name('home');
-
-// Google connection
-Route::get('/redirect', 'SocialAuthGoogleController@redirect');
-Route::get('/callback', 'SocialAuthGoogleController@callback');
+Route::get('/', 'HomeController@index')->name('home');
