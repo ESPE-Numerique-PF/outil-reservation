ad480f0377c0e1062e51fad9fccdd244dc94bd46
tehema.teiti <tehema.teiti@gmail.com>
2020-08-30 07:05:54 +0200

    add migration for user roles: Enseignant, Etudiant...

---
+++ database/migrations/2020_08_30_045411_create_user_roles_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Schema;
+
+class CreateUserRolesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('user_roles', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+        });
+
+        DB::table('user_roles')->insert(
+            [
+                ['id' => 1, 'name' => 'Enseignant'],
+                ['id' => 2, 'name' => 'Etudiant'],
+                ['id' => 3, 'name' => 'Autre'],
+            ]
+        );
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('user_roles');
+    }
+}
