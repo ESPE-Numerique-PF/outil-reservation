b73e2f04b1bcbd9ed1f91c389ad946080f114e0c
tehema.teiti <tehema.teiti@gmail.com>
2020-08-31 06:41:02 +0200

    add materials tables

--- database/migrations/2020_08_31_035343_create_categories_table.php
+++ database/migrations/2020_08_31_035343_create_categories_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateMaterielsTable extends Migration
+class CreateCategoriesTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,12 +13,9 @@ class CreateMaterielsTable extends Migration
      */
     public function up()
     {
-        Schema::create('materiels', function (Blueprint $table) {
+        Schema::create('categories', function (Blueprint $table) {
             $table->id();
             $table->string('name');
-            $table->text('description');
-            $table->text('notes');
-            $table->string('image_path');
             $table->timestamps();
         });
     }
@@ -30,6 +27,6 @@ class CreateMaterielsTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('materiels');
+        Schema::dropIfExists('categories');
     }
 }
