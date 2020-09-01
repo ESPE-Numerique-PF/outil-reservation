3d4f85942a5cc6cd084349534f6ad95d6ff2f027
ESPE NUMERIQUE <espenumerique@espe-boss.adupf.local>
2020-08-31 16:53:56 -1000

    jobs select in register form (in progress)

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -5,7 +5,7 @@
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
-                <div class="card-header">{{ __('Register') }}</div>
+                <div class="card-header">Création d'un nouveau compte</div>
 
                 <div class="card-body">
                     <form method="POST" action="{{ route('register') }}">
@@ -13,7 +13,7 @@
 
                         <!-- First Name -->
                         <div class="form-group row required">
-                            <label for="firstname" class="col-md-4 col-form-label text-md-right">{{ __('Prénom') }}</label>
+                            <label for="firstname" class="col-md-4 col-form-label text-md-right">Prénom</label>
 
                             <div class="col-md-6">
                                 <input id="firstname" type="text" class="form-control @error('firstname') is-invalid @enderror" name="firstname" value="{{ old('firstname') }}" required autocomplete="firstname" autofocus>
@@ -28,7 +28,7 @@
 
                         <!-- Last Name -->
                         <div class="form-group row required">
-                            <label for="lastname" class="col-md-4 col-form-label text-md-right">{{ __('Nom') }}</label>
+                            <label for="lastname" class="col-md-4 col-form-label text-md-right">Nom</label>
 
                             <div class="col-md-6">
                                 <input id="lastname" type="text" class="form-control @error('lastname') is-invalid @enderror" name="lastname" value="{{ old('lastname') }}" required autocomplete="lastname" autofocus>
@@ -43,7 +43,7 @@
 
                         <!-- Email -->
                         <div class="form-group row required">
-                            <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('E-mail') }}</label>
+                            <label for="email" class="col-md-4 col-form-label text-md-right">E-mail</label>
 
                             <div class="col-md-6">
                                 <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email">
@@ -58,7 +58,7 @@
 
                         <!-- Password -->
                         <div class="form-group row required">
-                            <label for="password" class="col-md-4 col-form-label text-md-right">{{ __('Mot de passe') }}</label>
+                            <label for="password" class="col-md-4 col-form-label text-md-right">Mot de passe</label>
 
                             <div class="col-md-6">
                                 <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="new-password">
@@ -72,16 +72,16 @@
                         </div>
 
                         <div class="form-group row required">
-                            <label for="password-confirm" class="col-md-4 col-form-label text-md-right">{{ __('Confirmation du mot de passe') }}</label>
+                            <label for="password-confirm" class="col-md-4 col-form-label text-md-right">Confirmation du mot de passe</label>
 
                             <div class="col-md-6">
                                 <input id="password-confirm" type="password" class="form-control" name="password_confirmation" required autocomplete="new-password">
                             </div>
                         </div>
 
-                        <!-- TODO Phone Number -->
+                        <!-- Phone Number -->
                         <div class="form-group row">
-                            <label for="phone_number" class="col-md-4 col-form-label text-md-right">{{ __('Téléphone') }}</label>
+                            <label for="phone_number" class="col-md-4 col-form-label text-md-right">Téléphone</label>
 
                             <div class="col-md-6">
                                 <input id="phone_number" type="text" class="form-control @error('phone_number') is-invalid @enderror" name="phone_number" autocomplete="new-phone_number">
@@ -93,12 +93,29 @@
                                 @enderror
                             </div>
                         </div>
-                        <!-- TODO User Role -->
+
+                        <!-- TODO User Job -->
+                        <div class="form-group row">
+                            <label for="phone_number" class="col-md-4 col-form-label text-md-right">Poste</label>
+
+                            <div class="col-md-6">
+                                <!-- <input id="phone_number" type="text" class="form-control @error('phone_number') is-invalid @enderror" name="phone_number" autocomplete="new-phone_number">
+
+                                @error('phone_number')
+                                <span class="invalid-feedback" role="alert">
+                                    <strong>{{ $message }}</strong>
+                                </span>
+                                @enderror -->
+
+                                <!-- @foreach ()
+                                $endforeach -->
+                            </div>
+                        </div>
 
                         <div class="form-group row mb-0">
                             <div class="col-md-6 offset-md-4">
                                 <button type="submit" class="btn btn-primary">
-                                    {{ __('Register') }}
+                                    Enregistrer
                                 </button>
                             </div>
                         </div>
