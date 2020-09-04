51e11ea1294198bba2796541dde570fd2d1e04ba
ESPE NUMERIQUE <espenumerique@espe-boss.adupf.local>
2020-09-03 15:45:14 -1000

    include font-awesome in vuejs main file (resources/js/app.js)

--- resources/js/components/admin/MaterialManagementComponent.vue
+++ resources/js/components/admin/MaterialManagementComponent.vue
@@ -3,7 +3,7 @@
     <h1>Gestion du matériel</h1>
     <div class="row my-3">
       <div class="col">
-        <a href="#" class="btn btn-primary">Ajouter une catégorie</a>
+        <a href="#" class="btn btn-primary"><span class="fa fa-search"></span> Ajouter une catégorie</a>
         <a href="#" class="btn btn-primary">Ajouter un matériel</a>
       </div>
     </div>
