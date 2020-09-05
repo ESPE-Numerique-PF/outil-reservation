0d468d023653a4ef0159e287890a2d39da15e798
tehema.teiti <tehema.teiti@gmail.com>
2020-09-05 09:08:55 +0200

    translate auth, pagination and passwords php files in french

---
+++ resources/lang/fr/passwords.php
@@ -0,0 +1,22 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Password Reset Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are the default lines which match reasons
+    | that are given by the password broker for a password update attempt
+    | has failed, such as for an invalid token or invalid new password.
+    |
+    */
+
+    'reset' => 'Votre mot de passe a été réinitialisé.',
+    'sent' => 'Le lien de réinitialisation de votre mot de passe vous a été envoyé par mail.',
+    'throttled' => 'Veuillez patienter quelques instants avant de réessayer.',
+    'token' => 'Le jeton de réinitialisation du mot de passe est invalide.',
+    'user' => "L'utilisateur associé à cet email est introuvable.",
+
+];
