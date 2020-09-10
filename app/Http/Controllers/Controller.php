4da4aec6f71ac0ef5aeb0718c0a28a8393530ed9
ESPE NUMERIQUE <espenumerique@espe-boss.adupf.local>
2020-09-10 08:30:18 -1000

    add helper function to write debug log in debug.log file

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -6,10 +6,16 @@ use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 use Illuminate\Foundation\Bus\DispatchesJobs;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Routing\Controller as BaseController;
+use Illuminate\Support\Facades\Log;
 
 class Controller extends BaseController
 {
     use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
 
     const NO_IMAGE_PATH = "images/no_image.png";
+
+    public static function debug($message)
+    {
+        Log::channel('debug')->debug($message);
+    }
 }
