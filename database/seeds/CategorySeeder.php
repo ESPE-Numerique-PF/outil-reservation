1a9cdf163159166fdb09eaffdef23d8ce9711042
ESPE NUMERIQUE <espenumerique@espe-boss.adupf.local>
2020-09-15 16:35:22 -1000

    add category seeder

---
+++ database/seeds/CategorySeeder.php
@@ -0,0 +1,54 @@
+<?php
+
+use App\Category;
+use Illuminate\Database\Seeder;
+
+class CategorySeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $category = [];
+
+        // Informatique
+        $category['informatique'] = Category::create([
+            'name' => 'Informatique'
+        ]);
+
+        $category['ordinateur'] = Category::create([
+            'name' => 'Ordinateur',
+            'parent_category_id' => $category['informatique']
+        ]);
+
+        $category['ipad'] = Category::create([
+            'name' => 'Ipad',
+            'parent_category_id' => $category['informatique']
+        ]);
+
+        // Accessoire
+        $category['accessoire'] = Category::create([
+            'name' => 'Accessoire',
+            'parent_category_id' => $category['informatique']
+        ]);
+
+        $category['cable'] = Category::create([
+            'name' => 'Cable',
+            'parent_category_id' => $category['accessoire']
+        ]);
+
+        $category['adaptateur'] = Category::create([
+            'name' => 'Adaptateur',
+            'parent_category_id' => $category['accessoire']
+        ]);
+
+        $category['adaptateur_hdmi_vga'] = Category::create([
+            'name' => 'Adaptateur HDMI/VGA',
+            'parent_category_id' => $category['accessoire']
+        ]);
+        
+    }
+}
